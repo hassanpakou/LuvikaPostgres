b703ad8171b191aa7076ba29afec3d0d6db561bc
hassanpakou <phakunestor@gmail.com>
2026-02-16 01:41:00 +0100

    mise à jour v2.1.0 Fait à Kinshasa avec ♥ des images logo + dimensions

--- src/app/_document.tsx
+++ src/app/_document.tsx
@@ -4,11 +4,24 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="fr">
-      <Head />
+      <Head>
+        {/* Favicon classique */}
+        <link rel="icon" href="/icons/lo.png" sizes="32x32" type="image/png" />
+        <link rel="shortcut icon" href="/icons/lo.png" />
+
+        {/* Manifest PWA */}
+        <link rel="manifest" href="/manifest.json" />
+
+        {/* Apple touch icon */}
+        <link rel="apple-touch-icon" href="/icons/lo-512.png" />
+
+        {/* Meta theme color pour Chrome et Android */}
+        <meta name="theme-color" content="#06b6d4" />
+      </Head>
       <body>
         <Main />
         <NextScript />
-        {/* ✅ Nettoie les attributs parasites avant hydration */}
+        {/* Nettoie les attributs parasites avant hydration */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
